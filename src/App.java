091a57f6b1f811f0d6262ce8052ebfe094e07fbf
OnurynFFc <149428021+OnurynFFc@users.noreply.github.com>
2025-10-18 15:05:20 -0300

    estanciando

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        InsertionSort sorter = new InsertionSort();
+        int[] array = { 5, 2, 9, 1, 5, 6 };
+        sorter.insertionSort(array);
+        System.out.println("Sorted array: " + java.util.Arrays.toString(array));
     }
 }
